9c77f18d788c34c646deda0f4830ebcc86a7a0df
ilsong963 <ilsong963@naver.com>
2025-04-03 20:37:40 +0900

    feat: product register page write

---
+++ lib/ui/product_register/product_register_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class ProductRegisterPage extends StatelessWidget {
+  const ProductRegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
