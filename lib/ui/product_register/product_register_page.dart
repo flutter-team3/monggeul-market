b7ebf7fa1c00a6c4795f123f44e52648a8ad2a98
ilsong963 <ilsong963@naver.com>
2025-04-03 19:56:57 +0900

    add: product register page

---
+++ lib/ui/product_register/product_register_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductRegisterPage extends StatelessWidget {
+  const ProductRegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
