f38ce91ddc211abdd55a5a3052f08f659bd4d42e
daehan-lim <penjan.en@gmail.com>
2025-04-04 10:22:15 +0900

    refactor(product): 상품 클래스의 imageUrl 삭제하고 generateProductList() 삭제
    - 랜덤 이미지 링크를 접근할 때마다 어차피 새로운 이미지가 생성되니 이미지 ULR 속성이 소용 없을 거 같습니다
    - 앞으로 상품 리스트를 다음과 같이 호출하면 됩니다: List<Product> products = List.from(AppConstants.productList)

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -14,23 +14,6 @@ class Product {
     required this.description,
     required this.price,
     required this.category,
-    this.imageUrl,
+    // this.imageUrl,
   });
-
-  static List<Product> generateProductList() {  // Product.generateProductList() 로 호출. 나중에 다른 파일로 옮기기
-    final List<Product> productList = [];
-
-    for (int i = 0; i < AppConstants.baseProductList.length; i++) {
-      final baseProduct = AppConstants.baseProductList[i];
-
-      productList.add(Product(
-        name: baseProduct.name,
-        description: baseProduct.description,
-        price: baseProduct.price,
-        category: baseProduct.category,
-        imageUrl: 'https://picsum.photos/200/300?random=${i + 1}',
-      ));
-    }
-    return productList;
-  }
 }
