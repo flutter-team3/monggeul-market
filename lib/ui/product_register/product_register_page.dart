aaef51ed16626da23b0575f250be0a01aef2f715
ilsong963 <ilsong963@naver.com>
2025-04-04 11:16:20 +0900

    feat: SafeArea적용

--- lib/ui/product_register/product_register_page.dart
+++ lib/ui/product_register/product_register_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ProductRegisterPage extends StatelessWidget {
   const ProductRegisterPage({super.key});
@@ -8,16 +7,21 @@ class ProductRegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("TITLE"), centerTitle: true),
-      body: Column(
-        children: [
-          AspectRatio(aspectRatio: 16 / 9, child: Container(width: double.infinity, color: Colors.grey, child: Center(child: Text('이미지')))),
-          Row(children: [Text("상품 이름"), Expanded(child: TextField())]),
-          Row(children: [Text("상품 가격"), Expanded(child: TextField())]),
+      body: SafeArea(
+        child: Column(
+          children: [
+            AspectRatio(aspectRatio: 16 / 9, child: Container(width: double.infinity, color: Colors.grey, child: Center(child: Text('이미지')))),
+            Row(children: [Text("상품 이름"), Expanded(child: TextField())]),
+            Row(children: [Text("상품 가격"), Expanded(child: TextField())]),
 
-          Text("상품 설명"),
-          Expanded(child: TextField()),
-          GestureDetector(child: Container(color: Colors.grey, height: 70, width: double.infinity, child: Center(child: Text("등록하기"))), onTap: () {}),
-        ],
+            Text("상품 설명"),
+            Expanded(child: TextField()),
+            GestureDetector(
+              child: Container(color: Colors.grey, height: 70, width: double.infinity, child: Center(child: Text("등록하기"))),
+              onTap: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
