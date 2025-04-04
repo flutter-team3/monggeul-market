ce3b46a9e88a6d5d98849ecd12287169e24d7a73
daehan-lim <penjan.en@gmail.com>
2025-04-04 14:27:23 +0900

    feat(util): 숫자를 원화로 포멧팅하는 함수 구현. 재사용 가능
    - pull 한 후에 전에 flutter pub get 명령어 실행 필요

---
+++ lib/util/util.dart
@@ -0,0 +1,8 @@
+import 'package:intl/intl.dart';
+
+/// 숫자를 ₩원화 형식으로 변환합니다.
+/// 예: formatKrw(15000) → ₩15,000
+String formatKrw(int number) {
+  final formatCurrency = NumberFormat.currency(locale: 'ko_KR', symbol: '₩');
+  return formatCurrency.format(number);
+}
