3217477509ce9f94d711446ae754527b40eb9bdb
daehan-lim <penjan.en@gmail.com>
2025-04-07 15:40:31 +0900

    style: 상세 페이지에서 CartItemAmount의 더하기 빼기 버튼의 탭 효과가 적용 안 되고 있어서 수정 및 버튼 텍스트 크기 조정

--- lib/ui/product_detail/product_detail_page.dart
+++ lib/ui/product_detail/product_detail_page.dart
@@ -55,85 +55,93 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
               ),
             ),
           ),
-          Container(
-            height: 150,
-            decoration: BoxDecoration(color: Colors.grey[300]),
-            child: Column(
-              children: [
-                SizedBox(height: 15),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: CartItemAmount(
-                          amount: amount,
-                          onPlusIconTap: () {
-                            setState(() {
-                              amount++;
-                            });
-                          },
-                          onMinusIconTap: () {
-                            amount > 1
-                                ? setState(() {
-                                  amount--;
-                                })
-                                : {};
-                          },
-                        ),
-                      ),
-                      SizedBox(width: 25),
-                      Expanded(
-                        child: Text(
-                          formatKrw(product.price),
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
+          Material(
+            color: Colors.grey[300],
+            child: SizedBox(
+              height: 150,
+              child: Column(
+                children: [
+                  SizedBox(height: 15),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: CartItemAmount(
+                            amount: amount,
+                            onPlusIconTap: () {
+                              setState(() {
+                                amount++;
+                              });
+                            },
+                            onMinusIconTap: () {
+                              amount > 1
+                                  ? setState(() {
+                                    amount--;
+                                  })
+                                  : {};
+                            },
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 15),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: () {
-                            showAppCupertinoDialog(
-                              context: context,
-                              title: '성공',
-                              content: "${product.name} $amount개를 장바구니에 담았습니다.",
-                            );
-                            CartProvider.of(
-                              context,
-                            ).addProductToCart(product, amount);
-                          },
-                          child: Text('장바구니 담기'),
+                        SizedBox(width: 25),
+                        Expanded(
+                          child: Text(
+                            formatKrw(product.price),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
-                      ),
-                      SizedBox(width: 25),
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: () {
-                            showAppCupertinoDialog(
-                              showCancel: true,
-                              context: context,
-                              title: '구매 확인',
-                              content: '${product.name} $amount개 구매하시겠습니까?',
-                            );
-                          },
-                          child: Text('구매하기'),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                  SizedBox(height: 15),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              showAppCupertinoDialog(
+                                context: context,
+                                title: '성공',
+                                content: "${product.name} $amount개를 장바구니에 담았습니다.",
+                              );
+                              CartProvider.of(
+                                context,
+                              ).addProductToCart(product, amount);
+                            },
+                            child: Text(
+                                '장바구니 담기',
+                                style: TextStyle(fontSize: 15,
+                                    fontWeight: FontWeight.bold)),
+                          ),
+                        ),
+                        SizedBox(width: 25),
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              showAppCupertinoDialog(
+                                showCancel: true,
+                                context: context,
+                                title: '구매 확인',
+                                content: '${product.name} $amount개 구매하시겠습니까?',
+                              );
+                            },
+                            child: Text(
+                                '구매하기',
+                                style: TextStyle(fontSize: 15,
+                                    fontWeight: FontWeight.bold)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
