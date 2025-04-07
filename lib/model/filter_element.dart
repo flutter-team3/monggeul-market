f856fef912fb3332e96cee9a91acc2d4237b19dd
kihyun <=>
2025-04-07 17:41:53 +0900

    refactor:category null처리를 위해 nullable수정

--- lib/model/filter_element.dart
+++ lib/model/filter_element.dart
@@ -2,6 +2,6 @@ import 'package:project_name_change/model/category.dart';
 
 class FilterElement {
   String word;
-  Category category;
+  Category? category;
   FilterElement(this.word, this.category);
 }
