9c56a2250bf27288534a43f00be9d532e60721e0
ilsong963 <ilsong963@naver.com>
2025-04-08 18:44:58 +0900

    chore: splash 배경색 변경

--- lib/app/constants/app_colors.dart
+++ lib/app/constants/app_colors.dart
@@ -4,6 +4,6 @@ class AppColors {
   static const primary = Color(0xFF4A2618);
   static const lightBrown = Color(0xFF74512D);
   static const yellow = Color(0xFFFEBA17);
-  static const newBackground = Color.fromARGB(255, 252, 243, 231); // Color(0xFFF8F4E1);
+  static const newBackground = Color(0xFFFCF3E7); // Color(0xFFF8F4E1);
   static const oldBackground = Color(0xFFF8F4E1);
 }
