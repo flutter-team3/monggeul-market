00cb9544271bc4aae42216287e0ebeb49f4e6833
ilsong963 <ilsong963@naver.com>
2025-04-08 12:20:50 +0900

    style: 제품 상세 페이지 앱바 중앙 정렬

--- lib/ui/product_detail/product_detail_page.dart
+++ lib/ui/product_detail/product_detail_page.dart
@@ -22,16 +22,12 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
     final product = ModalRoute.of(context)!.settings.arguments as Product;
 
     return Scaffold(
-      appBar: AppBar(title: Text('제품 상세')),
+      appBar: AppBar(title: Text('제품 상세'), centerTitle: true),
       body: Column(
         children: [
           AspectRatio(
             aspectRatio: 3 / 2,
-            child: AppCachedImage(
-              imageUrl:
-                  '${AppConstants.randomImageUrl}seed/${product.imageSeed}/300/200',
-              fit: BoxFit.cover,
-            ),
+            child: AppCachedImage(imageUrl: '${AppConstants.randomImageUrl}seed/${product.imageSeed}/300/200', fit: BoxFit.cover),
           ),
 
           SizedBox(height: 10),
@@ -41,10 +37,7 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
               children: [
                 Text((product.name), style: TextStyle(fontSize: 24)),
                 Expanded(child: SizedBox()),
-                Text(
-                  formatKrw(product.price),
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                ),
+                Text(formatKrw(product.price), style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
               ],
             ),
           ),
@@ -52,11 +45,7 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
           Expanded(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: ListView(
-                children: [
-                  Text(product.description, style: TextStyle(fontSize: 16)),
-                ],
-              ),
+              child: ListView(children: [Text(product.description, style: TextStyle(fontSize: 16))]),
             ),
           ),
           Material(
@@ -76,10 +65,7 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                               Text(
                                 '총 ${formatKrw(product.price * amount)}',
                                 textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 26,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
                               ),
                               Expanded(child: SizedBox()),
                               CartItemAmount(
@@ -114,60 +100,32 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                         Expanded(
                           child: ElevatedButton(
                             onPressed: () {
-                              showAppCupertinoDialog(
-                                context: context,
-                                title: '성공',
-                                content:
-                                    "${product.name} $amount개를 장바구니에 담았습니다.",
-                              );
-                              CartProvider.of(
-                                context,
-                              ).addProductToCart(product, amount);
+                              showAppCupertinoDialog(context: context, title: '성공', content: "${product.name} $amount개를 장바구니에 담았습니다.");
+                              CartProvider.of(context).addProductToCart(product, amount);
                             },
-                            child: Text(
-                              '장바구니 담기',
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
+                            child: Text('장바구니 담기', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
                           ),
                         ),
                         SizedBox(width: 25),
                         Expanded(
                           child: ElevatedButton(
                             onPressed: () async {
-                              final String?
-                              result = await showAppCupertinoDialog(
+                              final String? result = await showAppCupertinoDialog(
                                 showCancel: true,
                                 context: context,
                                 title: '구매 확인',
-                                content:
-                                    '${product.name} $amount개 구매하시겠습니까? 장바구니로 이동합니다.',
+                                content: '${product.name} $amount개 구매하시겠습니까? 장바구니로 이동합니다.',
                               );
                               if (!context.mounted) {
                                 return;
                               } // await 후에 context 안전한 사용
                               if (result == '확인') {
-                                CartProvider.of(
-                                  context,
-                                ).addProductToCart(product, amount);
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => CartPage(),
-                                  ),
-                                );
+                                CartProvider.of(context).addProductToCart(product, amount);
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => CartPage()));
                               }
                             },
 
-                            child: Text(
-                              '구매하기',
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
+                            child: Text('구매하기', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
                           ),
                         ),
                       ],
